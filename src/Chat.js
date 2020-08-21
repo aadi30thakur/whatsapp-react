8f4c8179311a372a57e1db22a28fa890610837ad
aditya thakur <aadi30thakur@github.com>
2020-08-21 23:53:07 +0530

    todays's work over

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 import "./chat.css";
 function Chat() {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 500000));
+  }, []);
+
   return (
     <div className="chat">
-      <div className="chat_header"></div>
-      <div className="chat_body"></div>
+      <div className="chat_header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+        <div className="chat_headerInfo">
+          <h3>roomname</h3>
+          <p>last seen at...</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">
+        <p className="chat_message">
+          <span className="chat_name">aadi</span>
+          hey guys
+        </p>
+      </div>
       <div className="chat_footer"></div>
     </div>
   );
