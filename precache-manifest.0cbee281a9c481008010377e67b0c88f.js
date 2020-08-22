0c0fcfb617612fe679f334956a5675d4636955d5
aditya thakur <aadi30thakur@github.com>
2020-08-22 23:50:28 +0530

    Updates

---
+++ precache-manifest.0cbee281a9c481008010377e67b0c88f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c45272ba351c0affd0ce6945c75668b",
+    "url": "/whatsapp-react/index.html"
+  },
+  {
+    "revision": "cd779e6f5c1d9259e236",
+    "url": "/whatsapp-react/static/css/main.ec47d793.chunk.css"
+  },
+  {
+    "revision": "a8e1277bec1f8f6f6168",
+    "url": "/whatsapp-react/static/js/2.4ce4a679.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-react/static/js/2.4ce4a679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd779e6f5c1d9259e236",
+    "url": "/whatsapp-react/static/js/main.23f9c93d.chunk.js"
+  },
+  {
+    "revision": "1c955cebea638554cbd4",
+    "url": "/whatsapp-react/static/js/runtime-main.11e3007d.js"
+  }
+]);
