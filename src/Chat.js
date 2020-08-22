d097a305545f6ac8a4e8ac37592af13e415e12f6
aditya thakur <aadi30thakur@github.com>
2020-08-22 13:47:31 +0530

    ui completed

--- src/Chat.js
+++ src/Chat.js
@@ -3,13 +3,20 @@ import { Avatar, IconButton } from "@material-ui/core";
 import AttachFile from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import "./chat.css";
 function Chat() {
   const [seed, setSeed] = useState("");
-
+  const [input, setinput] = useState("");
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 500000));
   }, []);
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("you typed >>>>>>>>>>>>>", input);
+    setinput("");
+  };
 
   return (
     <div className="chat">
@@ -33,12 +40,27 @@ function Chat() {
         </div>
       </div>
       <div className="chat_body">
-        <p className="chat_message">
+        <p className={`chat_message ${true && "chat_reciver"} `}>
           <span className="chat_name">aadi</span>
           hey guys
+          <span className="chat_timestamp">3.52 pm</span>
         </p>
       </div>
-      <div className="chat_footer"></div>
+      <div className="chat_footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setinput(e.target.value)}
+            placeholder="type a message"
+          />
+          <button type="submit" onClick={sendMessage}>
+            send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALMFSn5ZlQwNs0l5zx-vGo-YTiQWoLRA8",
+  authDomain: "whatsapp-react-28b4b.firebaseapp.com",
+  databaseURL: "https://whatsapp-react-28b4b.firebaseio.com",
+  projectId: "whatsapp-react-28b4b",
+  storageBucket: "whatsapp-react-28b4b.appspot.com",
+  messagingSenderId: "525199461092",
+  appId: "1:525199461092:web:41d34bfd0605d423c47af1",
+  measurementId: "G-LLD64X3TPT",
+};
